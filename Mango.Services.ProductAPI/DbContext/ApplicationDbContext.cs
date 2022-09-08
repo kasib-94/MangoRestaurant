0f9c2ca1848cc8faf2e4905a72fa0aa06488b67c
kasib-94 <jakczarski@gmail.com>
2022-09-08 20:59:01 +0200

    adding DbContext, Product.cs Model

---
+++ Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.ProductAPI.Models;
+
+namespace Mango.Services.ProductAPI.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+    public DbSet<Product> Products { get; set; }
+}
--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,7 +1,12 @@
+using Mango.Services.ProductAPI.DbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException(
+                           "Connection string 'ApplicationDbContextConnection' not found.");
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
