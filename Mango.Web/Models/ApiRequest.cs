b85cf2123c1f6dcd7904c4d24b791a0edef5f09d
kasib-94 <jakczarski@gmail.com>
2022-09-10 22:42:41 +0200

    implementing base service, api request

---
+++ Mango.Web/Models/ApiRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Models;
+using static Mango.Web.SD;
+
+public class ApiRequest
+{
+    public ApiType ApiType { get; set; } = ApiType.GET;
+    public string Url { get; set; }
+    public object Data { get; set; }
+    public string AccessToken { get; set; }
+}
